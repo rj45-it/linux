98dffbb31ceb9437779c608cf95bafe448f67eb8
wojtekojtek <49075395+wojtekojtek@users.noreply.github.com>
2026-07-09 16:03:59 +0200

    i2c: qcom-cci: enable clocks sequentially with cci_src rate set in-order

--- drivers/i2c/busses/i2c-qcom-cci.c
+++ drivers/i2c/busses/i2c-qcom-cci.c
@@ -469,7 +469,27 @@ static const struct i2c_algorithm cci_algo = {
 
 static int cci_enable_clocks(struct cci *cci)
 {
-	return clk_bulk_prepare_enable(cci->nclocks, cci->clocks);
+	int i, ret;
+
+	for (i = 0; i < cci->nclocks; i++) {
+		if (!strcmp(cci->clocks[i].id, "cci_src")) {
+			ret = clk_set_rate(cci->clocks[i].clk, cci->data->cci_clk_rate);
+			if (ret)
+				goto err_disable;
+		}
+
+		ret = clk_prepare_enable(cci->clocks[i].clk);
+		if (ret)
+			goto err_disable;
+	}
+
+	return 0;
+
+	err_disable:
+	for (--i; i >= 0; i--)
+		clk_disable_unprepare(cci->clocks[i].clk);
+
+	return ret;
 }
 
 static void cci_disable_clocks(struct cci *cci)
@@ -608,15 +628,6 @@ static int cci_probe(struct platform_device *pdev)
 		 */
 		dev_warn(dev, "Found %lu cci clk rate while %lu was expected\n",
 			 cci_clk_rate, cci->data->cci_clk_rate);
-	}
-
-	for (i = 0; i < cci->nclocks; i++) {
-		if (!strcmp(cci->clocks[i].id, "cci_src")) {
-			ret = clk_set_rate(cci->clocks[i].clk, cci->data->cci_clk_rate);
-			if (ret)
-				return dev_err_probe(dev, ret, "failed to set cci_src rate\n");
-			break;
-		}
 	}
 
 	pr_err("cci-debug: about to enable clocks for %s\n", dev_name(dev));
